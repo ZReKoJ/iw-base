9a0c819093c0611b9bba61a4f96ed811e45de106
RKJMTW <zhong@ucm.es>
2018-05-20 12:43:11 +0200

    Problem with undefined battleGround solved

--- src/main/resources/static/js/playing.js
+++ src/main/resources/static/js/playing.js
@@ -16,54 +16,54 @@ function playing(map, code, enemies) {
 		battleGround.fillContent(data, function(){
 			start(battleGround, new Array(code).concat(enemies));
 		});
-	});
-
-	canvas.addEventListener('mousemove', function(event) {
-		let rect = canvas.getBoundingClientRect();
-		let x = Math.floor((event.clientX - rect.left) * (canvas.width / rect.width));
-        let y = Math.floor((event.clientY - rect.top) * (canvas.height / rect.height));
-        battleGround.mouseAt = battleGround.defineMouseAt(x, y);
 		
-	    battleGround.clear().drawMapContent();
-	    battleGround.drawCell(battleGround.mouseAt.cellPosition.x, battleGround.mouseAt.cellPosition.y);
+		canvas.addEventListener('mousemove', function(event) {
+			let rect = canvas.getBoundingClientRect();
+			let x = Math.floor((event.clientX - rect.left) * (canvas.width / rect.width));
+	        let y = Math.floor((event.clientY - rect.top) * (canvas.height / rect.height));
+	        battleGround.mouseAt = battleGround.defineMouseAt(x, y);
+			
+		    battleGround.clear().drawMapContent();
+		    battleGround.drawCell(battleGround.mouseAt.cellPosition.x, battleGround.mouseAt.cellPosition.y);
+		    
+		    event.returnValue = false;
+		});
+
+		canvas.addEventListener('wheel', function(event){
+			
+			if (event.deltaY < 0) battleGround.zoomIn();
+			else battleGround.zoomOut();
+			
+	        let oldTable = battleGround.table;
+	        battleGround.table = new Rectangle(battleGround.cols * battleGround.cell.width, battleGround.rows * battleGround.cell.height);
+	        battleGround.mapCenter.relativeLocation(oldTable, battleGround.table);
+	        battleGround.defineMapFeature();
+	        battleGround.clear().drawMapContent();
+		    battleGround.drawCell(battleGround.mouseAt.cellPosition.x, battleGround.mouseAt.cellPosition.y);
+		    
+		    event.returnValue = false;
+		    
+		});
+
+		window.onresize = function() {
+			
+			if (isFullScreen()) {
+				canvas.width = window.innerWidth;
+				canvas.height = window.innerHeight;
+			}
+			else {
+				canvas.width = parent.width();
+				canvas.height = parent.width();
+			}
+			
+			battleGround.reset(canvas);
+		    battleGround.clear().drawMapContent();
 	    
-	    event.returnValue = false;
-	});
+	    };
 
-	canvas.addEventListener('wheel', function(event){
-		
-		if (event.deltaY < 0) battleGround.zoomIn();
-		else battleGround.zoomOut();
-		
-        let oldTable = battleGround.table;
-        battleGround.table = new Rectangle(battleGround.cols * battleGround.cell.width, battleGround.rows * battleGround.cell.height);
-        battleGround.mapCenter.relativeLocation(oldTable, battleGround.table);
-        battleGround.defineMapFeature();
-        battleGround.clear().drawMapContent();
-	    battleGround.drawCell(battleGround.mouseAt.cellPosition.x, battleGround.mouseAt.cellPosition.y);
-	    
-	    event.returnValue = false;
-	    
-	});
-
-	window.onresize = function() {
-		
-		if (isFullScreen()) {
-			canvas.width = window.innerWidth;
-			canvas.height = window.innerHeight;
-		}
-		else {
-			canvas.width = parent.width();
-			canvas.height = parent.width();
-		}
-		
-		battleGround.reset(canvas);
-	    battleGround.clear().drawMapContent();
-    
-    };
-
-	document.getElementById("fullscreen").addEventListener("click", function(){
-		fullscreen(canvas);
+		document.getElementById("fullscreen").addEventListener("click", function(){
+			fullscreen(canvas);
+		});
 	});
 
 }
